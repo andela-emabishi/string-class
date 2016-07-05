3a2dbe8a8ee991f36f9027a387591475403e8b46
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-07-05 15:38:08 +0300

    Employ style review with airbnb

--- jasmine/spec/string-class-spec.js
+++ jasmine/spec/string-class-spec.js
@@ -1,12 +1,10 @@
 describe('String Class Extension tests', () => {
-
   describe('Test hasVowels', () => {
     it('should return false if the string has no vowels', () => {
       expect('N VWLS'.hasVowels()).toBeFalsy();
       expect('n vwls'.hasVowels()).toBeFalsy();
       expect('@#$^&*%12345'.hasVowels()).toBeFalsy();
       expect(' '.hasVowels()).toBeFalsy();
-
     });
 
     it('should return true if the string has vowels', () => {
@@ -14,15 +12,12 @@ describe('String Class Extension tests', () => {
       expect('NO VOWELS'.hasVowels()).toBeTruthy();
       expect('n0 v0wels'.hasVowels()).toBeTruthy();
       expect('@#$^&*% 12345 e'.hasVowels()).toBeTruthy();
-
     });
 
     it('should return a defined and boolean value', () => {
       expect(typeof ('1234'.hasVowels())).toBe('boolean');
       expect(typeof ('1234'.hasVowels())).toBeDefined();
-
     });
-
   });
 
   describe('Test toUpper', () => {
@@ -35,7 +30,6 @@ describe('String Class Extension tests', () => {
     it('should return a string', () => {
       expect(typeof 'Lowercase'.toUpper()).toBe('string');
     });
-
   });
 
   describe('Test toLower', () => {
@@ -48,7 +42,6 @@ describe('String Class Extension tests', () => {
     it('should return a string', () => {
       expect(typeof 'Lowercase'.toLower()).toBe('string');
     });
-
   });
 
   describe('Test upperCaseFirst', () => {
@@ -61,7 +54,6 @@ describe('String Class Extension tests', () => {
     it('should return a string', () => {
       expect(typeof 'Uppercase'.ucFirst()).toBe('string');
     });
-
   });
 
   describe('Test isQuestion', () => {
@@ -69,27 +61,23 @@ describe('String Class Extension tests', () => {
       expect('Is this a question?'.isQuestion()).toBeTruthy();
       expect('This is not a question'.isQuestion()).toBeFalsy();
       expect('Is this a question?'.isQuestion()).toBeDefined();
-
     });
-
   });
 
   describe('Test words', () => {
     it('should return a list of the words in the string, as an array', () => {
       expect('A rose by any other name'.words()).toBeDefined();
-      expect('A rose by any other name'.words()).toEqual(['A','rose','by','any','other','name']);
-      expect('A:rose,by?any!other$name'.words()).toEqual(['A','rose','by','any','other','name']);
+      expect('A rose by any other'.words()).toEqual(['A', 'rose', 'by', 'any', 'other']);
+      expect('A:rose,by?any!other'.words()).toEqual(['A', 'rose', 'by', 'any', 'other']);
       expect('rose'.words()).toEqual(['rose']);
       expect('rose'.words()).toEqual(['rose']);
       expect(''.words()).toEqual(['']);
-
     });
 
     it('should verify that the result of the words method is an array object', () => {
       expect(typeof 'A rose by any other name'.words()).toBe('object');
       expect(Array.isArray('A rose by any other name'.words())).toBeTruthy();
     });
-
   });
 
   describe('Test wordCount', () => {
@@ -97,7 +85,6 @@ describe('String Class Extension tests', () => {
       expect('A rose by any other name'.wordCount()).toEqual(6);
       expect('A rose by any other name'.wordCount()).toBeDefined();
       expect(typeof 'A rose by any other name'.wordCount()).toBe('number');
-
     });
   });
 
@@ -106,7 +93,6 @@ describe('String Class Extension tests', () => {
       expect('11111.11'.toCurrency()).toEqual('11,111.11');
       expect('11111'.toCurrency()).toEqual('11,111.00');
       expect('21111111.11'.toCurrency()).toEqual('21,111,111.11');
-
     });
 
     it('should return a string', () => {
@@ -116,18 +102,15 @@ describe('String Class Extension tests', () => {
   });
 
   describe('Test fromCurrency', () => {
-
     it('should return a number representation of the currency string', () => {
       expect('11,111.11'.fromCurrency()).toEqual(11111.11);
       expect('11111'.fromCurrency()).toEqual(11111.00);
       expect('21111111.11'.fromCurrency()).toEqual(21111111.11);
-  
     });
 
     it('should return a number', () => {
       expect(typeof '11,111.11'.fromCurrency()).toBe('number');
       expect('11,111.11'.fromCurrency()).toBeDefined();
     });
-
   });
 });
