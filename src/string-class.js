5bca72090f312cf96a4cf509d957851bab9b2d7d
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-07-08 19:48:44 +0300

    Add comments

--- src/string-class.js
+++ src/string-class.js
@@ -2,11 +2,16 @@
 /* eslint-disable no-extend-native */
 
 String.prototype.hasVowels = function hasVowels() {
+  // Is there any vowel in the input string?
   const vowels = new RegExp('[aeiou]', 'i');
   return vowels.test(this);
 };
 
 String.prototype.toUpper = function toUpper() {
+  /* Replace all lowercase letters in the input string with their uppercase
+  * analogs by converting each letter's ASCII value to decimal then back to
+  * ASCII
+  */
   const upper = new RegExp('[a-z]', 'g');
   return this.replace(upper, function transform(letter) {
     return String.fromCharCode(letter.charCodeAt(0) - 32);
@@ -21,16 +26,23 @@ String.prototype.toLower = function toLower() {
 };
 
 String.prototype.ucFirst = function ucFirst() {
+  /* Convert the fist character of the input string to uppercase then concatenate
+  * the rest of the string with the uppercase character
+  */
   return this[0].toUpper() + this.slice(1);
 };
 
 String.prototype.isQuestion = function isQuestion() {
+  // Is the last letter of the input string a question mark?
   const quest = new RegExp(/\?$/);
   return quest.test(this);
 };
 
 String.prototype.words = function words() {
-  const splitter = new RegExp(/\W+/);
+  /* Split the input string at any of the provided special characters
+  * and return an array of words
+  */
+  const splitter = new RegExp('[:," ";|?!().]');
   return this.split(splitter);
 };
 
@@ -39,6 +51,10 @@ String.prototype.wordCount = function wordCount() {
 };
 
 String.prototype.toCurrency = function toCurrency() {
+  /* (/d) -> Maches digits from 0-9 and remembers the particular digit match
+  * (/d)(?=(\d{3})+\.) -> Only matches a digit if it is followed by
+  * three other digits and if the entire string is a float
+  */
   const curr = new RegExp(/(\d)(?=(\d{3})+\.)/g);
   return (parseFloat(this).toFixed(2).replace(curr, '$1,'))
   .toString();
